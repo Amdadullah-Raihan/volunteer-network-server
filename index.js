7be615154bfbaf18a61067f8b9df30e0d9a7c7fb
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-02-14 16:22:14 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const port = process.env.port || 5000;
 const app = express();
+const cors = require('cors')
 
 
 //middleware
@@ -17,3 +19,19 @@ app.get('/',(req, res)=>{
 app.listen(port, (req,res)=>{
     console.log("Listening to port", port);
 })
+
+// connecting mongodb-atlas
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xplq2xb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+ try{
+    const database = client.db("volunteerNetwork");
+    const eventsCollection = database.collection("events");
+    console.log("Database connected")
+ }
+ finally{
+    await client.close();
+ }
+}
+run().catch(console.dir);
