741f07dabc3d5e654219175bbb4b62a991459baa
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-02-16 00:37:26 +0600

    post and get api created for events

--- index.js
+++ index.js
@@ -10,28 +10,43 @@ app.use(cors());
 app.use(express.json());
 
 //GET API for Root
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send("Running Volunteer Network")
 
 })
 
 //Listen
-app.listen(port, (req,res)=>{
+app.listen(port, (req, res) => {
     console.log("Listening to port", port);
 })
 
 // connecting mongodb-atlas
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xplq2xb.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://volunteer-network-db:V4QSUSf1m9lpaBGt@cluster0.xplq2xb.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
- try{
-    const database = client.db("volunteerNetwork");
-    const eventsCollection = database.collection("events");
-    console.log("Database connected")
- }
- finally{
-    await client.close();
- }
+async function run() {
+    try {
+        const database = client.db("volunteerNetwork");
+        const eventsCollection = database.collection("events");
+        console.log("Database connected")
+        
+        app.post('/addEvent', async(req, res)=>{
+            console.log("hitting event api")
+            const event = req.body;
+            const result = await eventsCollection.insertOne(event);
+            res.send(result)
+
+        })
+        //Events get api
+        app.get('/events', async(req,res)=>{
+           const events = await eventsCollection.find({}).toArray();
+           console.log(events);
+           res.json(events)
+        })
+    }
+    finally {
+        // await client.close();
+    }
 }
 run().catch(console.dir);
